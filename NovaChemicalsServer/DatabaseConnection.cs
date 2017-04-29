dcd86703b09b39e772fd6263b7f1d10c097a7529
Dianna Yim <dianna.yim@gmail.com>
2017-04-28 18:26:38 -0600

    First commit

---
+++ NovaChemicalsServer/DatabaseConnection.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaChemicalsServer
+{
+    public class DatabaseConnection
+    {
+        private DatabaseConnection() { }
+
+        private string databaseName = "novachemicals";
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DatabaseConnection _instance = null;
+        public static DatabaseConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DatabaseConnection();
+
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            bool result = true;
+
+            if (Connection == null)
+            {
+                string connString = "Server=199.116.235.43; database=novachemicals; UID=root; password=3beaseuofc!";
+                connection = new MySqlConnection(connString);
+                connection.Open();
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NovaChemicalsServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaChemicalsServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbCon = DatabaseConnection.Instance();
+            Console.WriteLine(dbCon.IsConnect());
+
+            if (dbCon.IsConnect()) {
+                int counter = 0;
+                string line;
+
+                StreamReader file = new StreamReader("C:\\Users\\aselab\\Documents\\NovaChemData\\01-Metal-BaseCase.txt");
+
+                while ((line = file.ReadLine()) != null || counter == 10)
+                {
+                    if (counter > 0)
+                    {
+
+                    }
+
+                }
+            }
+        }
+    }
+}
